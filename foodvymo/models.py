eae16446d12bcdd257bee11f8927a686c7659dc6
shubham2637 <shubham37@outlook.com>
2021-06-17 02:30:20 +0530

    added Merchant model

--- foodvymo/models.py
+++ foodvymo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Merchant(models.Model):
+    resturantName = models.CharField(max_length=128)
+    contactName = models.CharField(max_length=128)
+    pincode = models.CharField(max_length=6)
+    location = models.CharField(max_length=256)
+    website = models.URLField(max_length=128)
+    phoneNumber = models.CharField(max_length=13)
+    averageDailyTransaction = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id} {self.resturantName} {self.contactName} {self.averageDailyTransaction}"
